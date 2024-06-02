c2ce152e715772c6ccab3dc72c68be6a1ba5b15c
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2024-06-02 19:43:37 +0100

    :)

---
+++ ex4.php
@@ -0,0 +1,11 @@
+<?php
+
+$vector1 = [2, 4, 6];
+$vector2 = [6, 2, 9];
+
+// Dot product
+$dotProduct = ($vector1[0] * $vector2[0]) + ($vector1[1] * $vector2[1]) + ($vector1[2] * $vector2[2]);
+
+echo $dotProduct;
+
+?>
